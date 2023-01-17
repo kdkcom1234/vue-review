989f3984133c9f4f4c65f8547ccbf56b8e2c28ba
Daekeun Ko <kdkcom1234@gmail.com>
2023-01-17 18:28:08 +0900

    라우팅 정보

---
+++ 4-extending/vue-project/src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { RouterLink, RouterView, useRoute, useRouter } from 'vue-router'
+
+const router = useRouter();
+const route = useRoute();
+
+const handleRedirect = (path: string) => {
+  router.push(path);
+}
+
+</script>
+
+<template>
+  <div>
+    <header>
+      <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <button @click="handleRedirect('/about')">go to about</button>
+      </nav>
+      {{ route.fullPath }}
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  gap: 8px;
+}
+</style>
